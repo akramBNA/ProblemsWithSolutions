487aabf3edc27d7bdc60c9929991275ef6221438
akramBNA <akrambenaoun1993@gmail.com>
2023-05-31 07:17:25 +0100

    first comm

---
+++ 04-FirstNonRepeatingCharacter.js
@@ -0,0 +1,10 @@
+// Description:
+
+//In a given string, try to find the first non repeating character
+//Example: FirstNonRepeatingChar("Hi my name is hannah")  ==> "y"
+
+//Solution:
+
+function FirstNonRepeatingChar(argument) {
+	// body...
+}
