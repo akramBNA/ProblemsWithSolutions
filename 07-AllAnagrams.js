4eae569c57be1078378319c1751c234c74816d5e
akramBNA <akrambenaoun1993@gmail.com>
2023-06-12 15:25:25 +0100

    07-to be solved

---
+++ 07-AllAnagrams.js
@@ -0,0 +1,10 @@
+// Description:
+
+// Given a single input string, write a function that produces all possible anagrams
+// of a string and outputs them as an array.
+
+// Solution:
+
+function allAbagrams(str) {
+	// body...
+}
