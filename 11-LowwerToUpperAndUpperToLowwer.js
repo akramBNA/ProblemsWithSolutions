ad45c6ad7c07a768da43235c7d8f974633100be4
akramBNA <akrambenaoun1993@gmail.com>
2023-06-15 22:08:23 +0100

    #11 added

---
+++ 11-LowwerToUpperAndUpperToLowwer.js
@@ -0,0 +1,10 @@
+// Description:
+
+//Write a JS Program that transform a lowwer case letter to upper case and vice versa.
+//Don't use the built in functions.
+
+// Solution:
+
+function LowwerToUpperAndUpperToLowwer(char) {
+	return String.fromCharCode((ch.charCodeAt(0))-32)
+}
