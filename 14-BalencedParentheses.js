dc35b8401f3ab7c5562eff586d152ec7f1a10fae
akramBNA <akrambenaoun1993@gmail.com>
2023-07-17 01:32:56 +0100

    #14 uploaded

--- 14-BalencedParentheses.js
+++ 14-BalencedParentheses.js
@@ -3,3 +3,32 @@
 // Create a JS program that verifies if the number of parentheses in a given string is balanced
 
 // Solution:
+
+const isBalanced = function (str) {
+    var leftParentheses="("
+    var rightParentheses=")"
+    var countLeft=0
+    var countRight=0
+
+if(typeof(str) === "string"){
+    for(var i=0;i<str.length; i++){
+        if(str[i] === leftParentheses ){
+            countLeft++
+        }
+        else if(str[i] === rightParentheses){
+        countRight++
+        }
+    } 
+
+    if( countRight === countLeft){
+        return true
+    }
+        
+        return false
+}
+else {
+    return "please enter a string"
+}
+
+    
+};
