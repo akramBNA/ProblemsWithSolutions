77b3b7886686adfbf732e7757575aba291e58cdb
akramBNA <akrambenaoun1993@gmail.com>
2023-07-18 23:36:57 +0100

    Problem #15

--- 15-FractionConverter.js
+++ 15-FractionConverter.js
@@ -2,4 +2,18 @@
 
 //Write a JS program that takes a number as its argument and returns a string that represents that number's simplified fraction.
 
-// Solution: 
+// Solution:
+
+var toFraction = function(number) {
+  var frac=0
+  var result=""
+  for(var i=1; i<10; i++){
+    if(Number.isInteger(number * i) === true){
+      frac = number *i
+      result = frac+"/"+i
+      return result
+    }
+  }
+
+};
+
