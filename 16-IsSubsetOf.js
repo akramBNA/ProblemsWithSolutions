58cd0b0d605eec5a760fdbcc97748dbe664445da
akramBNA <akrambenaoun1993@gmail.com>
2023-07-20 23:11:52 +0100

    #16 uploaded

--- 16-IsSubsetOf.js
+++ 16-IsSubsetOf.js
@@ -3,3 +3,21 @@
 // Write a JS program that verifies is a set is a subset of another larger set or not!
 
 // Solution:
+
+var theSubset=["toygame","warmup"];
+
+Array.prototype.isSubsetOf = function (arr) {
+    var counter=0
+    for(var i=0; i<theSubset.length;i++){
+      for(var j=0;j<arr.length;j++){
+        if(theSubset[i] === arr[j]){
+          counter++
+        }
+      }
+    }
+    if(counter === theSubset.length){
+      return true
+    } else {
+      return false
+    }
+};
