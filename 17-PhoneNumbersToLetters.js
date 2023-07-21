d62ea0a93963f555b343bfa2432cbd7b02d9b5de
akramBNA <akrambenaoun1993@gmail.com>
2023-07-22 00:20:48 +0100

    #17 problem uploaded

--- 17-PhoneNumbersToLetters.js
+++ 17-PhoneNumbersToLetters.js
@@ -3,3 +3,16 @@
 //Write a JS program that converts telephone keypad numbers press to letters (for short messaging SMS)
 
 // Solution:
+
+const phoneDigitsToLetters = {
+  0: '0',
+  1: '1',
+  2: 'ABC',
+  3: 'DEF',
+  4: 'GHI',
+  5: 'JKL',
+  6: 'MNO',
+  7: 'PQRS',
+  8: 'TUV',
+  9: 'WXYZ'
+};
