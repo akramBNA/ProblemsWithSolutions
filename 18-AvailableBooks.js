3135bb5df9bbe920985182fdb4f1f0af621073be
akramBNA <akrambenaoun1993@gmail.com>
2023-07-26 21:52:27 +0100

    #18 uploaded

--- 18-AvailableBooks.js
+++ 18-AvailableBooks.js
@@ -5,3 +5,30 @@
 
 // Solution:
 
+class Book{
+	constructor(title,author,ISBN,NbrCopies){
+		this.title = title;
+		this.author = author;
+		this.ISBN = ISBN;
+		this.NbrCopies = NbrCopies;
+	}
+	get availability(){
+		return this.getAvailability();
+	}
+
+	getAvailability(){
+		if(this.NbrCopies === 0){
+			return "Out OF STOCK!"
+		} else if(this.NbrCopies <10){
+			return "LOW STOCK!"
+		}
+		return "IN STOCK!"
+	}
+
+	sell(NbrCopiesSold =1){
+		this.NbrCopies -= NbrCopiesSold;
+	}
+	restock(NbrCopiesRestocked = 5){
+		this.NbrCopies += NbrCopiesRestocked;
+	}
+}
