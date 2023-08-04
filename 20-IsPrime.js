c7ed84c441f9d5d9dc13ca09261f0a6ca93990ca
akramBNA <akrambenaoun1993@gmail.com>
2023-08-04 22:33:59 +0100

    Problem #20

--- 20-IsPrime.js
+++ 20-IsPrime.js
@@ -2,4 +2,17 @@
 
 // Create a JS program that verifies if a number is prime or not
 
-//Solution
+//Solution:
+
+const isPrime = function(n) {
+  if (typeof n !== 'number' || n < 1 || n % 1 !== 0) {
+    // n isn't a number or n is less than 1 or n is not an integer
+    return false;
+  }
+  for(var i=2; i<= n; i++){
+    if( n%i === 0){
+      return false
+    }
+  } 
+  return true
+};
